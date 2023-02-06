d641e152f429598e04a228f2252bc3f2c4a530f8
diana <arreoladiana11@gmail.com>
2023-02-06 13:40:05 -0800

    Add tests for BoardList component

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import Board from './Board';
+import React from "react";
+import Board from "./Board";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 
-describe("Wave 01: ChatEntry", () => {
+describe("Board Component Tests", () => {
   beforeEach(() => {
     render(
       <div>
-        <Board
-          title="Fun Quotes"
-          owner="Supriya"
-          cards=""
-        />
-        <Board
-          title="Hiking Mantras"
-          owner="Megan"
-          cards=""
-        />
-      </div> 
+        <Board title="Fun Quotes" owner="Supriya" cards="" />
+      </div>
     );
   });
 
-  test("shows the title for the first board", () => {
+  test("shows the title", () => {
     expect(screen.getByText(/Fun Quotes/)).toBeInTheDocument();
   });
 
-  test("shows the title for the second board", () => {
-    expect(screen.getByText(/Hiking Mantras/)).toBeInTheDocument();
+  test("does not show the owner", () => {
+    expect(screen.queryByText(/Supriya/)).not.toBeInTheDocument();
   });
 });
