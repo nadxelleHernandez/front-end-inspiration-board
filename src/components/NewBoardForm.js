e99bf62590945ec397dd53894b1f9ba1a43fc5d6
diana <arreoladiana11@gmail.com>
2023-02-05 22:21:17 -0800

    Add basic board form

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+function NewBoardForm() {
+  // todo: On submit, call "props.addBoard" function
 
-function NewBoardForm () {
-    // todo: On submit, call "props.addBoard" functio
+  return (
+    <section>
+      <form>
+        <div>
+            <label htmlFor="title">Title:</label>
+            <input name="title" />
+        </div>
+        <div>
+            <label htmlFor="owner">Owner:</label>
+            <input name="owner" />
+        </div>
+        <input
+            type="submit"
+            value="Submit Query" />
+      </form>
+      <button>Hide New Board Form</button>
+    </section>
+    
+  );
 }
-   
-   
 export default NewBoardForm;
