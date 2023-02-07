74487530e20ce3f64eecbd689e33ae2209cc20f7
diana <arreoladiana11@gmail.com>
2023-02-07 11:01:30 -0800

    Update New Card form with bootstrap

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,54 +4,65 @@ import Form from 'react-bootstrap/Form';
 
 
 function NewCardForm({selectedBoard, addCard}) {
-    const [formFields, setFormFields] = useState({
-        message:"",
+  const [formFields, setFormFields] = useState({
+      message:"",
+  })
+
+  const [message, setMessage] = useState("")
+
+  // On submit, call API to create a new card under the selected board
+  const addCardonSubmit = (event) => {
+    //todo:
+    // make API call to update cards in the database
+    // also update selectedBoard state, then CardList component will get re-rendered
+    // or just call fetchCards function to reload?
+    event.preventDefault();
+    addCard({
+        message: formFields.message
     })
 
-    const [message, setMessage] = useState("")
+    setFormFields({message:""});
+    setMessage("");
+  };
 
-    // On submit, call API to create a new card under the selected board
-    const addCardonSubmit = (event) => {
-        //todo:
-        // make API call to update cards in the database
-        // also update selectedBoard state, then CardList component will get re-rendered
-        // or just call fetchCards function to reload?
-        event.preventDefault();
-        addCard({
-            message: formFields.message
-        })
+  const fieldsOnChange = (event)=>{
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
 
-        setFormFields({message:""});
-        setMessage("");
+    setFormFields({
+        ...formFields,
+        [fieldName]: fieldValue,
+    })
+
+    if (fieldName === "message"){
+        setMessage(fieldValue)
     };
+  }
 
-    const fieldsOnChange = (event)=>{
-        const fieldName = event.target.name;
-        const fieldValue = event.target.value;
-
-        setFormFields({
-            ...formFields,
-            [fieldName]: fieldValue,
-        })
-
-        if (fieldName === "message"){
-            setMessage(fieldValue)
-        };
-    }
-
-    return (
-        <form onSubmit={addCardonSubmit}>
-            <h3>Create a New Card</h3>
-            <div>
-                <label htmlFor="message">Message</label>
-                <input id="message" name="message" value={formFields.message} onChange={fieldsOnChange}/>
-            </div>
-            <h4>Preview:</h4>
-            <p>{message}</p>
-            <input type="submit" value="Submit"/>
-        </form>
-    );
+  return (
+    <section>
+      <h2>Create a New Card</h2>
+      <Form
+      aria-label="Create a New Card"
+      name="cardForm"
+      onSubmit={addCardonSubmit}>
+      <Form.Group>
+        <Form.Label htmlFor="message">Message</Form.Label>
+        <Form.Control
+            required
+            id="message"
+            name="message"
+            value={formFields.message}
+            onChange={fieldsOnChange}/>
+      </Form.Group>
+      <div className="form-preview">Preview:</div>
+      <p>{message}</p>
+      <Button className="submit-button" type="submit" value="Submit">
+        Submit Query
+      </Button> 
+    </Form>
+    </section>
+  );
 }
-
 
 export default NewCardForm;
