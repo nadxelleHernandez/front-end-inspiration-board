ebea4224fa07f17465270c9092fbee94c90b60b4
diana <arreoladiana11@gmail.com>
2023-02-09 10:40:49 -0800

    Add sort by button

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,6 +3,9 @@ import NoteCard from "./Card";
 import Card from 'react-bootstrap/Card';
 import { faNoteSticky } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import "./CardList.css"
 
 
 function CardList ({boardTitle, cards, updateLike, deleteCard}) {
@@ -18,7 +21,16 @@ function CardList ({boardTitle, cards, updateLike, deleteCard}) {
     return (
         <section>
             <Card>
-                <Card.Header><FontAwesomeIcon icon={faNoteSticky}/> Cards for <span className="font-italic">{boardTitle}</span></Card.Header>
+                <Card.Header className="d-flex flex-wrap">
+                    <div className="cardlist-title">
+                        <FontAwesomeIcon icon={faNoteSticky}/> Cards for <span className="font-italic">{boardTitle}</span>
+                    </div>
+                    <DropdownButton id="dropdown-basic-button" title="Sort by">
+                        <Dropdown.Item href="#/action-1">ID</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">A-Z</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Likes</Dropdown.Item>
+                    </DropdownButton>
+                </Card.Header>
                 <Card.Body className="d-flex flex-wrap">
                     {cardComponents}
                 </Card.Body>
